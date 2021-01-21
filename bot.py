a9948d4bb8cfb855c2593700aa80603407ab4aa7
Laljaka <watashiwakoroshitai@gmail.com>
2021-01-21 16:24:46 +0300

    Cleared the code from a ton of comments

--- bot.py
+++ bot.py
@@ -5,15 +5,12 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-#------------------------------------------------------------------------------
 #Variables
 intents = discord.Intents.default()
 intents.members = True
 restrictedTO = 798456032358694912
 
-#------------------------------------------------------------------------------
 #stuff
-
 def json_read(filename):
     with open(f'{filename}.json', 'r') as f:
         data = json.load(f)
@@ -24,8 +21,6 @@ def json_write(filename, data):
         json.dump(data, f, indent =4)
 
 def get_prefix(client, message):
-  #with open('storage.json', 'r') as f:
-    #prefixes = json.load(f)
     prefixes = json_read('storage')
     return prefixes[str(message.guild.id)]['prefix']
 
@@ -33,47 +28,33 @@ def get_prefix(client, message):
 client = commands.Bot(command_prefix = get_prefix, intents = intents)
 
 def check_roleassign_message(payload):
-    #with open('storage.json', 'r') as f:
     roleassign1 = json_read('storage')
     return roleassign1[str(payload.guild_id)]['rolemessage']
 
-#------------------------------------------------------------------------------
 #Ready check
-
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name='the screams of meatbags'))
     print('Bot is ready as {0.user}'.format(client))
 
-#------------------------------------------------------------------------------
 #Joining the server and creating a storage in json file
 @client.event
 async def on_guild_join(guild):
-    #with open('storage.json', 'r') as f:
     prefixes = json_read('storage')
-
     prefixes[str(guild.id)] = {'prefix': '?', 'rolemessage': None, 'emojis': []}
-
-
-    #with open('storage.json', 'w') as f:
     json_write('storage', prefixes)
 
-#------------------------------------------------------------------------------
 #Leaving the server and removing the storage
-
 @client.event
 async def on_guild_remove(guild):
-    #with open('storage.json', 'r') as f:
     prefixes = json_read('storage')
-
     prefixes.pop(str(guild.id))
-
-    #with open('storage.json', 'w') as f:
     json_write('storage', prefixes)
 
 #------------------------------------------------------------------------------
 @client.command()
 @commands.has_guild_permissions(administrator=True)
+@commands.guild_only()
 async def changeprefix(ctx, prefix):
     """
     Command to change bot prefix
@@ -81,14 +62,9 @@ async def changeprefix(ctx, prefix):
     if ctx.channel.id != restrictedTO:
         return
     else:
-        #with open('storage.json', 'r') as f:
         prefixes = json_read('storage')
-
         prefixes[str(ctx.guild.id)]['prefix'] = prefix
-
-        #with open('storage.json', 'w') as f:
         json_write('storage', prefixes)
-
         await ctx.send(f'Prefix changed to {prefix}')
 
 #------------------------------------------------------------------------------
@@ -114,50 +90,19 @@ async def create(message):
             return True
 
     roleassign1 = json_read('storage')
-
-
-
-        #await message.channel.send('Please specify the amount of emojis to roles you want to have')
-
-        #num = await client.wait_for('message', check=check)
-
-        #roleassign1[str(message.guild.id)]['emojis'] = [''] * int(num.content)
-
-        ###print(roleassign1[str(message.guild.id)]['emojis'])
-
-
     deletable = await message.channel.send('Please send the message')
-
     msgtorole = await client.wait_for('message', check=check)
-
-        #with open('storage.json', 'r') as f:
-
-
     roleassign1[str(message.guild.id)]['rolemessage'] = msgtorole.id
-
-
-        #await message.channel.send('Now please react with emojis you want to be associated with roles under your message, please note that amount of emojis should be the same as you just specified')
-
-        #i=0
-        #for run in roleassign1[str(message.guild.id)]['emojis']:
-        #    reaction = await client.wait_for('reaction_add')#ADD CHECK
-        #    roleassign1[str(message.guild.id)]['emojis'][i] = reaction[0].emoji.name
-        #    i=i+1
-
-        #def chek2(msg2):
-        #    if msg2.id == msgtorole.id:
-        #        return False
-        #    else:
-        #        return True
-        #with open('storage.json', 'w') as f:
-        #json.dump(roleassign1, f, indent=4)
     json_write('storage', roleassign1)
     await message.message.delete()
     await deletable.delete()
-        #await message.channel.purge(limit=6, check=chek2)
+    #await message.channel.purge(limit=6, check=chek2)
 
 @roleassign.command()
 async def add(message):
+    """
+    Command to add an emoji to roleassign message
+    """
     def check(msg):
         if msg.author.id == message.author.id:
             print('what the fuck')
@@ -167,8 +112,8 @@ async def add(message):
     guild = client.get_guild(message.guild.id)
     emoji = discord.utils.get(guild.emojis, name=emojirolename.content)
     role = discord.utils.get(guild.roles, name=emojirolename.content)
-    print(emoji)
-    print(role)
+    #print(emoji)
+    #print(role)
     if emoji != None and role != None:
         roleassign = json_read('storage')
         roleassign[str(message.guild.id)]['emojis'].append(emoji.name)
@@ -184,6 +129,9 @@ async def add(message):
 
 @roleassign.command()
 async def clear(message):
+    """
+    Command to clear emojis under roleassign message
+    """
     roleassign1 = json_read('storage')
     roleassign1[str(message.guild.id)]['emojis'] = []
     json_write('storage', roleassign1)
@@ -198,9 +146,8 @@ async def remove(message):
     roleassign1[str(message.guild.id)]['emojis'] = []
     json_write('storage', roleassign1)
 
-#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------ ADD TO ANOTHER FILE
 #Commands to add and remove roles by reacting to the message with emojis
-
 @client.event
 async def on_raw_reaction_add(payload):
     if payload.message_id == check_roleassign_message(payload) and payload.user_id != client.user.id:
@@ -225,6 +172,7 @@ async def on_raw_reaction_add(payload):
             await seek.remove_reaction(payload.emoji, member)
     else:
         return
+
 @client.event
 async def on_raw_reaction_remove(payload):
     if payload.user_id != client.user.id:
@@ -247,31 +195,24 @@ async def on_raw_reaction_remove(payload):
 @client.command()
 async def ping(ctx):
     """
-    Test command
+    Test command to get latency
     """
-    if ctx.channel.id != restrictedTO:
-        return
-    else:
-        await ctx.send(f'{round(client.latency * 1000)}ms')
+    await ctx.send(f'{round(client.latency * 1000)}ms')
 
-#------------------------------------------------------------------------------
 @client.command()
 @commands.has_guild_permissions(manage_messages=True)
+@commands.guild_only()
 async def purge(ctx, amount=100):
     """
-    Command to clean the messages
+    Command to clear messages (100 is default)
     """
-    if ctx.channel.id != restrictedTO:
-        return
-    else:
-        def check(msg):
-            if msg.id == ctx.message.id:
-                return False
-            else:
-                return True
-        await ctx.channel.purge(limit=amount, check=check)
+    def check(msg):
+        if msg.id == ctx.message.id:
+            return False
+        else:
+            return True
+    await ctx.channel.purge(limit=amount, check=check)
 
-#------------------------------------------------------------------------------
 #To do
 '''
 @client.group()
@@ -301,12 +242,14 @@ async def request(ctx):
     print(role)
     print(member)
 '''
+
 @client.command()
 @commands.is_owner()
 async def logout(ctx):
     await client.logout()
 
 @client.command()
+@commands.is_owner()
 async def test(ctx):
 #    roleassign1 = json_read('storage')
 #    print(roleassign1[str(ctx.guild.id)]['emojis'])
@@ -324,6 +267,6 @@ async def test(ctx):
 #        restrictedTO = ctx.channel.id
 #        print(restrictedTO)
 
-#------------------------------------------------------------------------------
+
 #run
 client.run(os.getenv("TOKEN"))
